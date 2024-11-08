d7316d0aeae6a450012abeaab12fd6d4fb2b4152
EngJao89 <eng_joao@outlook.com>
2024-11-08 20:48:50 -0300

    Initial Commit

---
+++ Auto Layout/Auto_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_LayoutApp.swift
+//  Auto Layout
+//
+//  Created by João Ricardo Martins Ribeiro on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Auto_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auto Layout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Auto Layout
+//
+//  Created by João Ricardo Martins Ribeiro on 08/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
