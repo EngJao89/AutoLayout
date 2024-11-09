3d822aa0ae1cbf50fceb05f1e7986f340ae5d96c
EngJao89 <eng_joao@outlook.com>
2024-11-08 21:18:08 -0300

    feat: inicia projeto de aprendizado de layout.

--- Auto Layout/ContentView.swift
+++ Auto Layout/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                print("Botão pressionado")
+            }) {
+                Text("Logar")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
