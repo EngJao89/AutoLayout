9d0081ed64dbb6bdfd1f00248b72ead995c48c42
EngJao89 <eng_joao@outlook.com>
2024-11-12 20:39:07 -0300

    feat: implementa dashboard de usuário logado.

--- Auto Layout/ContentView.swift
+++ Auto Layout/ContentView.swift
@@ -11,42 +11,50 @@ struct ContentView: View {
     @State private var texto: String = ""
     @State private var senha: String = ""
     @State private var lembrarUsuario: Bool = false
-    
+    @State private var isLoggedIn: Bool = false
+
     var body: some View {
-        VStack {
-            Text("Usuário")
-            TextField("Seu usuário...", text: $texto)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-            
-            Text("Senha")
-            SecureField("Sua Senha...", text: $senha)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-            
-            Toggle(isOn: $lembrarUsuario) {
-                Text("Lembrar de usuário")
-                    .foregroundColor(.black)
-            }
-            .padding()
-            
-            Button(action: {
-                print("Botão pressionado")
-                print("Lembrar usuário: \(lembrarUsuario)")
-            }) {
-                Text("Logar")
+        NavigationStack {
+            VStack {
+                Text("Usuário")
+                TextField("Seu usuário...", text: $texto)
                     .foregroundColor(.white)
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.orange)
+                    .background(Color(.systemGray6))
                     .cornerRadius(10)
+
+                Text("Senha")
+                SecureField("Sua Senha...", text: $senha)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+
+                Toggle(isOn: $lembrarUsuario) {
+                    Text("Lembrar de usuário")
+                        .foregroundColor(.white)
+                }
+                .padding()
+
+                NavigationLink(destination: DashboardView(), isActive: $isLoggedIn) {
+                    Button(action: {
+                        print("Botão pressionado")
+                        print("Lembrar usuário: \(lembrarUsuario)")
+                        isLoggedIn = true  // Navega para a DashboardView
+                    }) {
+                        Text("Logar")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.orange)
+                            .cornerRadius(10)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
             }
+            .padding()
+            .navigationTitle("Login")
         }
-        .padding()
     }
 }
 
